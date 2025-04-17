5b64313dd18b49bd757a6482ac9aa995e7488b8e
ValentinMecchia <valentinmecchia@gmail.com>
2025-04-16 21:13:09 -0300

    Vite config modified

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [
+      'localhost',
+      '.localhost',
+      'desarrollo-de-software-tp2.onrender.com'
+    ]
   },
 })
